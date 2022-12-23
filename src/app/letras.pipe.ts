478346e4ad9a00f2d86b26a8c4a43e14f2cd1789
David Malpica <dmalpica96@hotmail.com>
2022-12-22 18:30:21 -0600

    Fixed pipe data

--- src/app/letras.pipe.ts
+++ src/app/letras.pipe.ts
@@ -36,19 +36,18 @@ export class LetrasPipe implements PipeTransform {
           case 10: return 'dez';
         }
       }
-
-      switch(value) {
-        case 1: return 'uno';
-        case 2: return 'dos';
-        case 3: return 'tres';
-        case 4: return 'cuatro';
-        case 5: return 'cinco';
-        case 6: return 'seis';
-        case 7: return 'siete';
-        case 8: return 'ocho';
-        case 9: return 'nueve';
-        case 10: return 'diez';
-      }
+    }
+    switch(value) {
+      case 1: return 'uno';
+      case 2: return 'dos';
+      case 3: return 'tres';
+      case 4: return 'cuatro';
+      case 5: return 'cinco';
+      case 6: return 'seis';
+      case 7: return 'siete';
+      case 8: return 'ocho';
+      case 9: return 'nueve';
+      case 10: return 'diez';
     }
     return null;
   }
